b93c29afa425733e6fd0e8ff9ff300f67a8aad8a
d31220207 <d31220207@student.polije.ac.id>
2024-07-31 18:16:04 +0700

    Update Reverser.py

--- Reverser.py
+++ Reverser.py
@@ -123,10 +123,10 @@ html_template = '''
             box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
         }
         #convertButton {
-            background: #4a90e2;
+            background: #1f8b4c;
         }
         #convertButton:hover {
-            background: #357ab8;
+            background: #176839;
         }
         .area {
             background: #4e54c8;
@@ -137,7 +137,7 @@ html_template = '''
             top: 0;
             left: 0;
         }
-        .circles {
+        .icons {
             position: absolute;
             top: 0;
             left: 0;
@@ -145,75 +145,74 @@ html_template = '''
             height: 100%;
             overflow: hidden;
         }
-        .circles li {
+        .icons svg {
             position: absolute;
             display: block;
-            list-style: none;
-            width: 20px;
-            height: 20px;
-            background: rgba(255, 255, 255, 0.2);
+            width: 50px;
+            height: 50px;
             animation: animate 25s linear infinite;
             bottom: -150px;
+            fill: rgba(255, 255, 255, 0.2);
         }
-        .circles li:nth-child(1) {
+        .icons svg:nth-child(1) {
             left: 25%;
             width: 80px;
             height: 80px;
             animation-delay: 0s;
         }
-        .circles li:nth-child(2) {
+        .icons svg:nth-child(2) {
             left: 10%;
-            width: 20px;
-            height: 20px;
+            width: 60px;
+            height: 60px;
             animation-delay: 2s;
             animation-duration: 12s;
         }
-        .circles li:nth-child(3) {
+        .icons svg:nth-child(3) {
             left: 70%;
-            width: 20px;
-            height: 20px;
+            width: 70px;
+            height: 70px;
             animation-delay: 4s;
         }
-        .circles li:nth-child(4) {
+        .icons svg:nth-child(4) {
             left: 40%;
             width: 60px;
             height: 60px;
             animation-delay: 0s;
             animation-duration: 18s;
         }
-        .circles li:nth-child(5) {
+        .icons svg:nth-child(5) {
             left: 65%;
-            width: 20px;
-            height: 20px;
+            width: 50px;
+            height: 50px;
             animation-delay: 0s;
         }
-        .circles li:nth-child(6) {
+        .icons svg:nth-child(6) {
             left: 75%;
             width: 110px;
             height: 110px;
             animation-delay: 3s;
         }
-        .circles li:nth-child(7) {
+        .icons svg:nth-child(7) {
             left: 35%;
             width: 150px;
             height: 150px;
             animation-delay: 7s;
         }
-        .circles li:nth-child(8) {
+        .icons svg:nth-child(8) {
             left: 50%;
-            width: 25px;
-            height: 25px;
+            width: 55px;
+            height: 55px;
             animation-delay: 15s;
             animation-duration: 45s;
         }
-        .circles li:nth-child(9) {
+        .icons svg:nth-child(9) {
             left: 20%;
-            width: 15px;
-            height: 15px;
+            width: 45px;
+            height: 45px;
             animation-delay: 2s;
             animation-duration: 35s;
         }
-        .circles li:nth-child(10) {
+        .icons svg:nth-child(10) {
             left: 85%;
             width: 150px;
             height: 150px;
@@ -224,12 +223,10 @@ html_template = '''
             0% {
                 transform: translateY(0) rotate(0deg);
                 opacity: 1;
-                border-radius: 0;
             }
             100% {
                 transform: translateY(-1000px) rotate(720deg);
                 opacity: 0;
-                border-radius: 50%;
             }
         }
         .footer {
@@ -247,9 +244,9 @@ html_template = '''
         .copy-icon {
             cursor: pointer;
             vertical-align: middle;
-            margin-right: 15px; /* Increased from 5px to 15px */
-            font-size: 26px; /* Increased from 20px to 24px */
-            padding: 5px; /* Added padding around the icon */
+            margin-right: 15px;
+            font-size: 26px;
+            padding: 5px;
         }
         
         #result {
@@ -267,24 +264,23 @@ html_template = '''
             flex-grow: 1;
             text-decoration: none;
             color: #4a90e2;
-            padding-left: 10px; /* Added left padding to the URL text */
+            padding-left: 10px;
         }
     </style>
 </head>
 <body>
     <div class="area">
-        <ul class="circles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+        <div class="icons">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 210 210"><path d="M0,105C0,47.103,47.103,0,105,0c23.383,0,45.515,7.523,64.004,21.756l-24.4,31.696C133.172,44.652,119.477,40,105,40 c-35.841,0-65,29.159-65,65s29.159,65,65,65c28.867,0,53.398-18.913,61.852-45H105V85h105v20c0,57.897-47.103,105-105,105 S0,162.897,0,105z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 210 210"><path d="M0,105C0,47.103,47.103,0,105,0c23.383,0,45.515,7.523,64.004,21.756l-24.4,31.696C133.172,44.652,119.477,40,105,40 c-35.841,0-65,29.159-65,65s29.159,65,65,65c28.867,0,53.398-18.913,61.852-45H105V85h105v20c0,57.897-47.103,105-105,105 S0,162.897,0,105z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 210 210"><path d="M0,105C0,47.103,47.103,0,105,0c23.383,0,45.515,7.523,64.004,21.756l-24.4,31.696C133.172,44.652,119.477,40,105,40 c-35.841,0-65,29.159-65,65s29.159,65,65,65c28.867,0,53.398-18.913,61.852-45H105V85h105v20c0,57.897-47.103,105-105,105 S0,162.897,0,105z"/></svg>
+        </div>
     </div>
     <div class="container">
         <h1>URL Reverser</h1>
